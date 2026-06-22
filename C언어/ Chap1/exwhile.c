261627f85a04b316c8da148143287723ada6b74f
bigone822 <49789248+bigone822@users.noreply.github.com>
2026-06-22 10:30:44 +0900

    업로드 테스트

---
+++ C언어/ Chap1/exwhile.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int i = 1;
+
+	while (i <= 5) {
+		printf("%d\n", i);
+		i++;
+	}
+
+	return 0;
+}
---
+++ C언어/ Chap1/gugu.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	for (int i = 1; i <= 9; i++) {
+		printf("5 * %d = %d\n", i, i * 5);
+	}
+	return 0;
+}
